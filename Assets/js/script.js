7ec34ecd66a631dcafa544783e3433daa70a4d4c
aenz <andrewjensor@gmail.com>
2022-10-12 10:29:40 -0700

    finalizing Qs

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,29 +1,47 @@
 
-var questionNum;
-var corAnswer;
-var userAnswer;
-var beginQuiz;
-var timeLeft
-var showTimer
+var beginButton = document.createElement("button");
+beginButton.innerHTML = "Begin the Quiz";
+beginButton.setAttribute("id", "begin");
+document.getElementById("quiz").appendChild(beginButton);
+
+beginButton.addEventListener("click", startingQuiz)
 
 
-if (corAnswer == userAnswer){
-    var correctness = true
 
-} else {
-    var correctness = false;
-}
+var startingQuiz = function startQuiz(){
+    beginButton.getElementById("begin").remove()
+//     var timeleft = 60;
+//     var countdownTimer = setInterval(function(){
+//   if(timeleft <= 0){
+//     clearInterval(downloadTimer);
+//     document.getElementById("countdown").innerHTML = "Finished";
+//   } else {
+//     document.getElementById("quiz").appendChild() = timeleft + " seconds remaining";
+//   }
+//   timeleft -= 1;
+// }, 1000);
+  }
 
-function updateTimer () {
-    timeLeft = timeLeft - 1
-}
 
-document.getElementById("begin").addEventListener("click", beginQuiz);
 
-function beginQuiz(){
-    timeLeft = 60;
-    window.getElementById("quiz").write("timeLeft" + "seconds remaining")
-}
 
-document.setInterval(updateTimer, 1000);
+// var createTimer = document.createElement("p")
 
+
+// if (corAnswer == userAnswer){
+//     var correctness = true
+
+// } else {
+//     var correctness = false;
+// }
+
+var timeleft = 60;
+var countdownTimer = setInterval(function(){
+  if(timeleft <= 0){
+    clearInterval(downloadTimer);
+    document.getElementById("countdown").innerHTML = "Finished";
+  } else {
+    document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
+  }
+  timeleft -= 1;
+}, 1000);
